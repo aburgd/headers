1d8664c10507b3545b6ec0c33021d224f128f6ad
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-06-06 01:14:50 +0000

    changed ip var

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.set('port', port)
 
 app.get('/', function(req, res) {
   var language = req.headers["accept-language"]
-  var ipAdd = req.ip
+  var ip = req.headers["x-forwarded-for"] || req.ip;
   var os = process.platform
   
   res.send({
     'language': language,
-    'ip-address': ipAdd,
+    'ip-address': ip,
     'operating-system': os
     })
 })
