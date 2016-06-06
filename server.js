7d31296981fc2791ee550c9a1ef30752ad20436c
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-06-06 00:51:55 +0000

    changed ip var

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 var express = require("express"),
-    locale = require("locale");
+    locale = require("locale")
 
-var app = express();
-var port = process.env.PORT || 3500;
+var app = express()
+var port = process.env.PORT || 3500
 
-app.set('port', port);
+app.set('port', port)
 
 app.get('/', function(req, res) {
-  var language = req.headers["accept-language"];
-  var ipAdd = req.connection.remoteAddress;
-  var os = process.platform;
+  var language = req.headers["accept-language"]
+  var ipAdd = req.ip
+  var os = process.platform
   
   res.send({
     'language': language,
     'ip-address': ipAdd,
     'operating-system': os
-    });
-});
+    })
+})
 
 app.listen(port, function(port) {
-  console.log("Server is listening on: ${port}");
-});
+  console.log("Server is listening on: ${port}")
+})
